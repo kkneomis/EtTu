b3eda80e37a4d135839e2c7939faf447b3b26ea4
Simeon Kakpovi <skakpovi@gmail.com>
2018-05-20 17:29:20 -0400

    changes to app.py

--- app.py
+++ app.py
@@ -1,33 +1,40 @@
-import requests
+
+# Importing flask functions to be used 
+from flask import Flask, render_template, redirect, url_for, flash, request, session
+
+#local imports
+from caesar import make_caesar_challenge, make_rs_challenge 
+
+#just for me :)
 import json
-import re
-import string
-import random
-from bs4 import BeautifulSoup
+
+# Instantiating the flask Object
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    """Render the main page"""
+    return render_template("index.html")
 
 
-def get_random_text():
-	r = requests.get('http://www.randomtext.me/api/gibberish/p-1/25-45')
-	res = r.json()['text_out']
+@app.route('/processinput', methods=['GET', 'POST'])
+def processinput():
+	challenge_type =  int(request.form['challenge_type'])
+	text_length = int(request.form['text_length'])
+	number_of_problems = int(request.form['number_of_problems'])
 
-	soup = BeautifulSoup(res, 'lxml')
-	clear_text =  soup.text
+	if challenge_type == 1:
+		challenges = make_caesar_challenge(number_of_problems)
+		#print json.dumps(challenges, indent=4)
+		return render_template('pdf/answers.html', challenges=challenges)
+	elif challenge_type == 2:
+		challenges = make_rs_challenge(number_of_problems)
+		#print json.dumps(challenges, indent=4)
+		return render_template('pdf/answers.html', challenges=challenges)
+	else:
+		return redirect(url_for('home'))
 
-	return clear_text
+	
 
-
-def caesar(plaintext, shift):
-    alphabet = string.ascii_lowercase
-    shifted_alphabet = alphabet[shift:] + alphabet[:shift]
-    table = string.maketrans(alphabet, shifted_alphabet)
-    return plaintext.translate(table)
-
-
-sample_text = str(get_random_text())
-shift = random.randint(1,25)
-print caesar(sample_text, shift)
-print "shift, is", shift
-guess = int(raw_input("Guess the shift:"))
-while guess != shift:
-	guess = int(raw_input("Nah fam, guess again"))
-print "Congratulations!"
+if __name__ == "__main__":   
+	app.run(debug=True)
